3a8e9d9cad7c0987ae22285e44ff7f3ee5db5e6e
Duyle <duy4ipad@gmail.com>
2025-10-20 16:43:39 +0700

    upload

---
+++ 04. movement/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(30,30,50);
+  triangle(30,20,40,50,90,10);
+  circle( 100, 20, 40);
+
+  //for(let i = 0; i<400; i=i+20)
+  // let r = random(30); 
+  // circle(i, i+300, r);
+  //}
+
+if ( select ==0 )(
+  rgb = (255,0,0);
+) else ( 
+  rgb = (0, 255, 0);
+)
+
+
+  fill (rgb)
+  let i=i;
+  while(i<400)(
+    let r = random(50);
+    curcle(i, i+300, r); 
+    i++;
+  )
+}
