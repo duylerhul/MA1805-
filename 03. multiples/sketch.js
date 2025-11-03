9f74e933f9648b59e7ae58063a10fecfe30e3999
Duyle <duy4ipad@gmail.com>
2025-11-04 06:16:55 +0700

    upload

--- 03. multiples/sketch.js
+++ 03. multiples/sketch.js
@@ -1,23 +1,31 @@
-let theText = ["some", "text", "to", "display"];
+let theText = ["some", "Text", "To", "Display"];
 let word = theText[0];
 let i = 0;
 
 function setup() {
-  createCanvas (400,400);
-  frameRate(2);
+  createCanvas(400, 400);
+  frameRate(2); // The sketch updates 2 times per second
 }
 
-
-function draw(){
+function draw() {
   background(220);
 
-  textSize(32);
-  text(word, random(400), height/2);
+  // The text display
+  textSize(50);
+  // Displays the current 'word' at a random X position and the center Y position
+  text(word, random(400), height / 2);
 
-  word = theText[i]
-  i=i +1;
-  if (i>=theText.length){
-    i=0;
+  // Change the value of word
+  // 1. Get the next word from the list
+  word = theText[i];
+  
+  // 2. Advance the index (i)
+  i = i + 1;
+  
+  // 3. Check if the index is out of bounds and reset it (loop back to the start)
+  if (i >= theText.length) {
+    i = 0;
   }
-  console.log(i)
+  
+  // console.log(i); // This line logs the index to the console
 }
--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,7 +1,17 @@
+let img;
+
+function preload() {
+  // The preload() function runs before setup() and draw()
+  // It ensures the image is fully loaded before the sketch tries to display it.
+  img = loadImage('military.jpg');
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  image(img, 0, 0);
 }
